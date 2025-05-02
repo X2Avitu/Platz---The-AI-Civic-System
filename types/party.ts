354fe483db233047ae3f8995edd86252195596b6
Roshan <roshaniruku@gmail.com>
2025-05-02 09:07:07 -0400

    Pushing for Fork

--- types/party.ts
+++ types/party.ts
@@ -9,24 +9,3 @@ export interface Party {
     createdAt: string
     user: string | null // Add the user property
   }
-
-  export interface Location {
-    lat: number
-    lng: number
-  }
-  
-  export interface Attendee {
-    id: string
-    name: string
-    joinedAt: string
-    avatar: string
-  }
-  
-export interface User {
-  id: string
-  name: string
-  email: string
-  avatar: string
-  createdParties: string[]
-  joinedParties: string[]
-}
